34a0ee80b154fc8e3524d0191e09db91d9a8f460
Andrianto <baelluminate@gmail.com>
2014-01-25 00:51:38 +0700

    final

---
+++ Pieces.h
@@ -0,0 +1,13 @@
+#ifndef PIECES_H_INCLUDED
+#define PIECES_H_INCLUDED
+
+class Pieces
+{
+public:
+
+	int GetBlockType		(int pPiece, int pRotation, int pX, int pY);
+	int GetXInitialPosition (int pPiece, int pRotation);
+	int GetYInitialPosition (int pPiece, int pRotation);
+};
+
+#endif // PIECES_H_INCLUDED
